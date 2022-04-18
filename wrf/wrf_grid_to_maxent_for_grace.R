b9d256139bfc365d38f184600b2658482232069e
unknown <gdicecco@live.unc.edu>
2022-04-18 17:47:09 -0500

    write regrid function

--- wrf/wrf_grid_to_maxent_for_grace.R
+++ wrf/wrf_grid_to_maxent_for_grace.R
@@ -1,85 +1,119 @@
-setwd('/home/owen/research/wildfire/wrf/proc/')
-setwd('/home/owen/research/wildfire/wrf/proc/')
+#### Import packages ####
 
 library(ncdf4)
 library(sp)
 library(rgdal)
 library(raster)
-library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(sf)
 library(stars)
 library(colorspace)
 
+#### Set up directories ####
+
+wrf_dir <- "wrf/"
+wrf_out <- "wrf_out/"
+map_dir <- "map_data/"
+fig_dir <- "figs/"
+
+#### Regrid WRF to rasters ####
+
 #Generate fixed fields from a static file
 #Need a reference native WRF grid for regridding
-varin <- paste0('WRF_grid.nc')   #sample file
+varin <- paste0(wrf_dir, 'WRF_grid.nc')   #sample file
 ncid<-nc_open(varin,write=FALSE)
 
 #Load variables
 wrflat<-ncvar_get(ncid,"XLAT")
 wrflon<-ncvar_get(ncid,"XLONG")
 
-#Read in the WRF Summarized Files
-#vars=c("CANWAT", "PBLH", "PREC_ACC_NC", "PSFC", "Q2", "T2", "U10", "V10") 
+# Regrid fn: arguments - wrf variable to process, directory where summarized wrf files are, output directory for bricked regrid data
+regrid_wrf <- function(wrf_var = c("CANWAT", "PBLH", "PREC_ACC_NC", "PSFC", "Q2", "T2", "U10", "V10"),
+                       in_directory = wrf_dir,
+                       out_directory = wrf_out) {
 
-setwd('/home/owen/research/wildfire/wrf/proc/')
-wrf.base <- readRDS('Q2_historical_wrf_summary.RDS')
+  #Read in the WRF Summarized Files
+  #vars=c("CANWAT", "PBLH", "PREC_ACC_NC", "PSFC", "Q2", "T2", "U10", "V10") 
+  # moisture in canopy - veg fuel content; boundary layer height - thermal structure/vertical mixing; precip volumetric; psfc suface pressure
+  # q2 specific humidity; t2 temperature; u ew component of wind; v ns wind
+  # wind statistical summaries are unrelated - want all of these
+  # temperature annual range and max value
+  # humidity annual range and max value
+  
+  wrf.base <- readRDS(paste0(wrf_dir,paste0(wrf_var, '_historical_wrf_summary.RDS')))
+  
+  print("reading in file...")
+  
+  #Rebuild the WRF Lambert Cylindrical Grid
+  wrf.base$xlat<- apply(wrf.base, 1, function(x) wrflat[x['east'],x['nor']])
+  wrf.base$xlon<- apply(wrf.base, 1, function(x) wrflon[x['east'],x['nor']])
+  
+  #Convert the data.frame to a spatial data frame
+  # coordinates(wrf.base) = ~xlon+xlat # this errors
+  
+  #Convert irregular WRF grid to spatial pixels data frame
+  pixels <- SpatialPixelsDataFrame(data.frame(wrf.base$xlon, wrf.base$xlat), tolerance = 0.916421, wrf.base)
+  #proj4string(pixels) = CRS("+proj=lcc +lat_1=28 +lat_2=50 +lat_0=39.700012 +lon_0=-98.0 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m ")
+  proj4string(pixels) = CRS("+proj=lcc +lat_1=28 +lat_2=50 +lat_0=29.9 +lon_0=-130.5 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m ") #passing the middle of grid from points2grid
+  
+  #This works!
+  rg <- raster(ncols=329,nrows=400) #create a raster to project onto
+  extent(rg) <- c(min(wrflon),max(wrflon),min(wrflat),max(wrflat))
+  
+  #rasterize the object - now converted to typical lat-lon grid
+  median.raster <- rasterize(pixels,rg,field='median')
+  mean.raster <- rasterize(pixels,rg,field='mean')
+  IQR.raster <- rasterize(pixels,rg,field='IQR')
+  sd.raster <- rasterize(pixels,rg,field='sd')
+  mad.raster <- rasterize(pixels,rg,field='mad')
+  min.raster <- rasterize(pixels,rg,field='min')
+  max.raster <- rasterize(pixels,rg,field='max')
+  q5.raster <- rasterize(pixels,rg,field='q5')
+  q95.raster <- rasterize(pixels,rg,field='q95')
+  
+  print("layers rasterized...")
+  
+  #If all NA then skip
+  #problems with NAs - only for CANWAT (or other variables which are mostly zeros)
+  
+  if(sum(is.na(pixels$kurtosis)) == 0) {
+    kurtosis.raster <- rasterize(pixels,rg,field='kurtosis')
+    skewness.raster <- rasterize(pixels,rg,field='skewness')
+    
+    out.brick <- brick(list(median.raster,mean.raster,IQR.raster,sd.raster,mad.raster,min.raster,max.raster,q5.raster,q95.raster,
+                            kurtosis.raster, skewness.raster))
+    names(out.brick) <- c('median','mean','IQR','sd','mad','min','max','q5','q95', 'kurtosis', 'skewness') 
+    
+    writeRaster(out.brick, filename=paste0(wrf_out, wrf_var, '_regrid.grd'), format="raster", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+    
+    print("file written")
+    
+  } else {
+    out.brick <- brick(list(median.raster,mean.raster,IQR.raster,sd.raster,mad.raster,min.raster,max.raster,q5.raster,q95.raster))
+    names(out.brick) <- c('median','mean','IQR','sd','mad','min','max','q5','q95') 
+    
+    writeRaster(out.brick, filename=paste0(wrf_out, wrf_var, '_regrid.grd'), format="raster", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+    
+    print("file written")
+  }
+  
+  #test plot
+  #plot(max.raster)
+  
+  }
 
-#Rebuild the WRF Lambert Cylindrical Grid
-wrf.base$xlat<- apply(wrf.base, 1, function(x) wrflat[x['east'],x['nor']])
-wrf.base$xlon<- apply(wrf.base, 1, function(x) wrflon[x['east'],x['nor']])
 
-#Convert the data.frame to a sptaial data frame
-coordinates(wrf.base) = ~xlon+xlat
-
-#Convert irregular WRF grid to spatial pixels data frame
-pixels <- SpatialPixelsDataFrame(wrf.base, tolerance = 0.916421, wrf.base@data)
-#proj4string(pixels) = CRS("+proj=lcc +lat_1=28 +lat_2=50 +lat_0=39.700012 +lon_0=-98.0 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m ")
-proj4string(pixels) = CRS("+proj=lcc +lat_1=28 +lat_2=50 +lat_0=29.9 +lon_0=-130.5 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m ") #passing the middle of grid from points2grid
-
-wrf.trans = spTransform(pixels,geo_proj)
-
-#This works!
-rg <- raster(ncols=329,nrows=400) #create a raster to project onto
-extent(rg) <- c(min(wrflon),max(wrflon),min(wrflat),max(wrflat))
-
-#rasterize the object - now converted to typical lat-lon grid
-median.raster <- rasterize(pixels,rg,field='median')
-mean.raster <- rasterize(pixels,rg,field='mean')
-IQR.raster <- rasterize(pixels,rg,field='IQR')
-sd.raster <- rasterize(pixels,rg,field='sd')
-mad.raster <- rasterize(pixels,rg,field='mad')
-min.raster <- rasterize(pixels,rg,field='min')
-max.raster <- rasterize(pixels,rg,field='max')
-q5.raster <- rasterize(pixels,rg,field='q5')
-q95.raster <- rasterize(pixels,rg,field='q95')
-
-#Grace here a logic test is needed - if all NA then skip
-#problems with NAs - only for CANWAT (or other variables which are mostly zeros)
-kurtosis.raster <- rasterize(pixels,rg,field='kurtosis')
-skewness.raster <- rasterize(pixels,rg,field='skewness')
-
-#test plot
-#plot(max.raster)
-
-out.brick <- brick(list(median.raster,mean.raster,IQR.raster,sd.raster,mad.raster,min.raster,max.raster,q5.raster,q95.raster))
-names(out.brick) <- c('median','mean','IQR','sd','mad','min','max','q5','q95') 
-
-
-writeRaster(out.brick, filename='FileNAme.grd', format="raster", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+## Apply regrid to all vars
+regrid_wrf("Q2")
 
 #read command for later
-ImportName <- brick("FileNAme.grd") 
+ImportName <- brick(paste0(wrf_out, "FileNAme.grd"))
 
-
-#############################################################
-#Grace: I can't remember if this code works
+#### Figure output #####
 
 #Sample Figure Output
 # Cut to a single domain
-setwd('/home/owen/research/wildfire/rds/')
-firewx.sf <- readRDS(file="firewx.sf.RDS") #stand alone fire wx areas (no info about IOU territories)
+firewx.sf <- readRDS(file=paste0(map_data, "firewx.sf.RDS")) #stand alone fire wx areas (no info about IOU territories)
 
 location.string = "Northwest"
 region_shape = firewx.sf %>%
@@ -92,12 +126,6 @@ region.raster <- rasterize(region_shape,IQR.raster)
 cropped.raster <- raster::mask(skewness.raster,region.raster)
 plot(cropped.raster)
 
-
-
-#############################
-#Make a proper plot for presentation
-############################
-
 #convert raster to a spatial data frame for plotting
 test_spdf <- as(cropped.raster, "SpatialPixelsDataFrame")
 test_df <- as.data.frame(test_spdf)
@@ -107,10 +135,12 @@ t = 5
 thres.string = "all"
 
 #Mapping assets
-setwd('/home/owen/research/electricity/hadISD/')
-load(file = "map_objects.RData")
+load(file = paste0(map_dir, "map_objects.RData"))
 
-setwd('~/research/wildfire/march2021/figs/')
+shape.extent <- extent(region_shape)
+fr <- st_rasterize(region_shape,crs = st_crs(IQR.raster))
+
+region_layer <- st_crop(x=st_as_stars(IQR.raster), y=fr, crop=TRUE)    #create a poly-raster
 
 #Plot all stations
 labtext = paste("WRF Similarity Score For ",location.string)
@@ -139,38 +169,16 @@ a <- ggplot(data = CA, aes(x=long, y = lat)) +
   labs(title=labtext,caption=captext,subtitle=subtext, x="Longitude",y="Latitude")
 
 a
-ggsave(file=fout, plot = a, device = "png", dpi = 300) 
+ggsave(file=paste0(fig_dir, fout), plot = a, device = "png", dpi = 300) 
 
 
-shape.extent <- extent(region_shape)
-fr <- st_rasterize(region_shape,st_crs(IQR.raster))
-
-rasterize(fr)
-
-region_layer <- st_crop(x=st_as_stars(IQR.raster), mask=fr, crop=TRUE)    #create a poly-raster
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#### Scratch code #####
 
 
 plot(test.raster,xlim=c(-150,-100),ylim=c(29,50))
 
 
+wrf.trans = spTransform(pixels,geo_proj)
 wrf.trans = projectRaster(test.raster, crs=geo_proj, method="bilinear")
 
 out<-rasterize(wrf.trans$max)
